2a2624a0e3fb465b8fb8d12ec95a43388aa3fd0c
akshayindeevaram <109133493+akshayindeevaram@users.noreply.github.com>
2022-07-21 15:58:54 +0530

    upadated auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,12 @@
-// const { Router } = require("express")
-
-// app.post("/registration"),
-// app.listen(4000)
-
-// // app.get("http://localhost:3002/registrationform", (req,res) => {
-// //     console.log("linked");
-// //     res.send("linked")
-// // })
-
-// module.exports = Router
-
-
 const express = require("express");
-const { register, login, verifyToken } = require("../controllers/register");
+const { register, login } = require("../controllers/register");
+
 const Router = express.Router()
-Router.route("/register").post(register)
-Router.route("/login", verifyToken).post(login)
+
+Router.route("/register").post(register);
+// Router.route("/login").post(login);
+Router.post('/login',login);
+
 
 
 module.exports = Router;
-
-
-
-
-
